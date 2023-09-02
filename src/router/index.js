0a19cb1358b613f475454ca55f132e68835876a5
yiyu7699 <yiyu7699@uni.sydney.edu.au>
2023-09-02 14:01:02 +1000

    chat room

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import VueRouter from 'vue-router'
 import admin from '../components/AdminDashboard/Admin.vue'
 import fun from '../components/Fun'
 import customer from "@/components/Customer";
-import chatRoom from "@/components/ChatRoom"
+import chatRoom from "../components/ChatRoom/ChatRoom.vue"
 import login from "../components/Login/login.vue"
 import register from "../components/Register/Register.vue";
 import store from '../store'; // 导入Vuex store
@@ -57,7 +57,7 @@ router.beforeEach((to, from, next) => {
     // 检查是否需要验证权限
     if (to.matched.some(record => record.meta.requiresAuth)) {
         if (!isLoggedIn) {
-            next('/login'); // 未登录则跳转到登录页
+            next('/'); // 未登录则跳转到登录页
         } else if ( to.meta.allowedRoles.some(item => role.includes(item))) {
             next(); // 放行角色和登录角色交叉就放行
         }else {
