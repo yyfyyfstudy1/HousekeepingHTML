caff628a40607e18dfa0fd4661e222cfffb85e1a
Zeqian Liu <zliu3006@uni.sydney.edu.au>
2023-10-03 15:57:53 +1100

    升级任务模板功能

--- src/components/EmployerPostJob/EmployerPostJob.js
+++ src/components/EmployerPostJob/EmployerPostJob.js
@@ -10,9 +10,58 @@ export default {
                 content: 'We are seeking experienced and diligent professionals for cleaning services in our residence. The tasks include general house cleaning, vacuuming, dusting, mopping, and bathroom cleaning.',
                 duration: 2,
                 salary: 50,
-                imageUrl: require("@/assets/img_3.png"),
-
+                imageUrl: require("@/assets/Cleaning_Services.jpg"),
             },
+            task2: {
+                title: 'Moving House',
+                category: 'Transportation and Moving',
+                taskId: 2,
+                content: 'We are seeking reliable individuals with a good driving record to help with transportation and moving. Responsibilities include helping move personal items, ensuring safe transportation, and driving to specified locations.',
+                duration: 3,
+                salary: 60,
+                imageUrl: require("@/assets/Transportation_Moving.jpeg"),
+            },
+
+            task3: {
+                title: 'Chef needed',
+                category: 'Food and Catering Services',
+                taskId: 3,
+                content: 'Looking for skilled chefs or caterers to provide quality food services for an upcoming event. Duties involve meal preparation, serving, and ensuring food hygiene standards.',
+                duration: 4,
+                salary: 70,
+                imageUrl: require("@/assets/Food_Catering.jpg"),
+            },
+
+            task4: {
+                title: 'Caring for the elderly',
+                category: 'Personal Care and Companionship',
+                taskId: 4,
+                content: 'We need compassionate and patient individuals to provide personal care and companionship services. Tasks may include assisting with daily activities, companionship, and ensuring the well-being of clients.',
+                duration: 3,
+                salary: 55,
+                imageUrl: require("@/assets/Personal_Care.jpg"),
+            },
+
+            task5: {
+                title: 'Business Support',
+                category: 'Business and Administrative Support',
+                taskId: 5,
+                content: 'In search of dedicated individuals with organizational skills to offer administrative support. Responsibilities encompass data entry, handling communications, and aiding with other business-related tasks.',
+                duration: 5,
+                salary: 65,
+                imageUrl: require("@/assets/Business_Support.jpg"),
+            },
+
+            task6: {
+                title: 'Hiring Sales',
+                category: 'Sales and Marketing',
+                taskId: 6,
+                content: 'Seeking dynamic and persuasive individuals for sales and marketing roles. Duties involve promoting products or services, strategizing campaigns, and achieving sales targets.',
+                duration: 4,
+                salary: 70,
+                imageUrl: require("@/assets/Sales_Marketing.jpg"),
+            },
+
             inputValue: '',
             inputTag:'',
             tags: [],
@@ -63,10 +112,32 @@ export default {
         },
         handleButtonClick1() {
             console.log("按钮被点击了！");
-            // this.$router.push({ path: "/PostJobDetail", query: { title: "Professional Cleaning Services"
-            // , describe: "We are seeking experienced and diligent professionals for cleaning services in our residence. The tasks include general house cleaning, vacuuming, dusting, mopping, and bathroom cleaning."
-            // , duration: 2, salary: 50} });
             this.$router.push({ path: "/PostJobDetail", query: { task: this.task1}});
         },
+        handleButtonClick2() {
+            console.log("按钮被点击了！");
+            this.$router.push({ path: "/PostJobDetail", query: { task: this.task2 }});
+        },
+
+        handleButtonClick3() {
+            console.log("按钮被点击了！");
+            this.$router.push({ path: "/PostJobDetail", query: { task: this.task3 }});
+        },
+
+        handleButtonClick4() {
+            console.log("按钮被点击了！");
+            this.$router.push({ path: "/PostJobDetail", query: { task: this.task4 }});
+        },
+
+        handleButtonClick5() {
+            console.log("按钮被点击了！");
+            this.$router.push({ path: "/PostJobDetail", query: { task: this.task5 }});
+        },
+
+        handleButtonClick6() {
+            console.log("按钮被点击了！");
+            this.$router.push({ path: "/PostJobDetail", query: { task: this.task6 }});
+        },
+
     }
 };
--- src/components/EmployerPostJob/EmployerPostJob.vue
+++ src/components/EmployerPostJob/EmployerPostJob.vue
@@ -23,49 +23,58 @@
 
       <el-card class="card" shadow="hover">
         <div class="card__body">
+          <img :src="task1.imageUrl" alt="Task Image" class="task-image">
           <h3>{{ task1.title }}</h3>
-          <p><strong>Task category:</strong> {{ task1.category }}</p>
-          <p><strong>Content:</strong> {{ task1.content }}</p>
-          <p><strong>Duration:</strong> {{ task1.duration }}</p>
-          <p><strong>Salary:</strong> {{ task1.salary }}</p>
-          <el-button @click="handleButtonClick1" class="card-button">Go</el-button>
+          <p><strong>Salary:</strong> ${{ task1.salary }}</p>
+          <el-button @click="handleButtonClick1" class="card-button">Post</el-button>
         </div>
       </el-card>
 
       <el-card class="card" shadow="hover">
         <div class="card__body">
-          <h3>Card Title</h3>
-          <p>Card description</p>
+          <img :src="task2.imageUrl" alt="Task Image" class="task-image">
+          <h3>{{ task2.title }}</h3>
+          <p><strong>Salary:</strong> ${{ task2.salary }}</p>
+          <el-button @click="handleButtonClick2" class="card-button">Post</el-button>
         </div>
       </el-card>
 
       <el-card class="card" shadow="hover">
         <div class="card__body">
-          <h3>Card Title</h3>
-          <p>Card description</p>
+          <img :src="task3.imageUrl" alt="Task Image" class="task-image">
+          <h3>{{ task3.title }}</h3>
+          <p><strong>Salary:</strong> ${{ task3.salary }}</p>
+          <el-button @click="handleButtonClick3" class="card-button">Post</el-button>
         </div>
       </el-card>
 
       <el-card class="card" shadow="hover">
         <div class="card__body">
-          <h3>Card Title</h3>
-          <p>Card description</p>
+          <img :src="task4.imageUrl" alt="Task Image" class="task-image">
+          <h3>{{ task4.title }}</h3>
+          <p><strong>Salary:</strong> ${{ task4.salary }}</p>
+          <el-button @click="handleButtonClick4" class="card-button">Post</el-button>
         </div>
       </el-card>
 
       <el-card class="card" shadow="hover">
         <div class="card__body">
-          <h3>Card Title</h3>
-          <p>Card description</p>
+          <img :src="task5.imageUrl" alt="Task Image" class="task-image">
+          <h3>{{ task5.title }}</h3>
+          <p><strong>Salary:</strong> ${{ task5.salary }}</p>
+          <el-button @click="handleButtonClick5" class="card-button">Post</el-button>
         </div>
       </el-card>
 
       <el-card class="card" shadow="hover">
         <div class="card__body">
-          <h3>Card Title</h3>
-          <p>Card description</p>
+          <img :src="task6.imageUrl" alt="Task Image" class="task-image">
+          <h3>{{ task6.title }}</h3>
+          <p><strong>Salary:</strong> ${{ task6.salary }}</p>
+          <el-button @click="handleButtonClick6" class="card-button">Post</el-button>
         </div>
       </el-card>
+
 
     </div>
   </div>
@@ -160,14 +169,14 @@
 }
 
 .card__body {
-  padding: 14px;
   position: relative;
 }
 
 .card-button {
   position: absolute;
-  right: 10px;
-  bottom: 10px;
+  right: 0px;
+  bottom: 0px;
+  padding-top: 10px;
   z-index: 1;
 }
 @media (max-width: 1000px) {
@@ -183,8 +192,17 @@
   }
   .card {
     width: calc(100% - 10px);
-    height:200px;
+    height:300px;
   }
 }
+.task-image {
+  width: 100%;     /* 充满整个容器的宽度 */
+  height: 200px;   /* 更改此处，固定高度为200px */
+  max-height: 50%;
+  display: block;  /* 使图片块级显示，占据整行 */
+  margin-bottom: 10px;  /* 图片和下面的内容之间的间距 */
+  object-fit: cover; /* 让图片保持纵横比并填充其容器 */
+}
+
 
 </style>
